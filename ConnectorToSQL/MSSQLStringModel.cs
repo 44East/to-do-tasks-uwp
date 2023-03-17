bd4bd618db9f0bc1efaa84a6a4583ad1a927a667
44East <antswert@gmail.com>
2023-03-17 18:18:31 +0300

    Add console app for insertion sql connection parameters, add library app, for generate connection string, add Get and Insert methods in head APP

---
+++ ConnectorToSQL/MSSQLStringModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ConnectorToSQL
+{
+    public class MSSQLStringModel
+    {
+        public MSSQLStringModel() { }
+        public MSSQLStringModel(string dataSource, string userId, string password)
+        {
+            DataSource = dataSource;
+            UserID = userId;
+            Password = password;
+        }
+        public string DataSource { get; set; }
+        public string UserID { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ConnectorToSQL/MySQLStringModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ConnectorToSQL
+{
+    public class MySQLStringModel
+    {
+        public MySQLStringModel() { }
+        public MySQLStringModel(string server, string userId, string password)
+        {
+            Server = server;
+            UserID = userId;
+            Password = password;
+        }
+        public string Server { get; set; }
+        public string UserID { get; set; }
+        public string Password { get; set; }
+    }
+}
